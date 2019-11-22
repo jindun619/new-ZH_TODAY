255cc38040e3b27fb7c1f2bd16f09c303557e69d
jindun <jindun619@naver.com>
2019-11-22 17:41:04 +0800

    2019-11-22_1

--- init.php
+++ init.php
@@ -1,3 +1,14 @@
 <?php
     painter(get_date());
+
+    $date = get_date();
+
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $query = "INSERT INTO visits (ip_address, visit_date) VALUES ('$ip_address', '$date')";
+    mysqli_query($conn, $query);
+
+    $query = "SELECT DISTINCT ip_address FROM visits WHERE visit_date = '$date'";
+    $result = mysqli_query($conn, $query);
+    $today_visits = mysqli_num_rows($result);
+    echo $today_visits;
 ?>
